3497f7de5a32ad66a54dfb960c4d5df522cc0490
saurabhbaraskar9 <srbaraskar9@gmail.com>
2024-12-14 12:43:52 +0530

    First Push

---
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -0,0 +1,17 @@
+package cucumber.Options;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/java/features",
+        plugin = {"json:target/jsonReports/cucumber-report.json"},
+        glue = {"stepDefinations"},
+        tags = "@Regression"
+)
+public class TestRunner {
+    // No additional fields or methods are required for this runner class
+}
